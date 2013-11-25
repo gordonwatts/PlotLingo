039409cdc8bede62ba7f10d602aacfbcba9e6475
Gordon Watts <gordon@gordonandpaula.net>
2013-11-24 20:03:48 -0800

    Inserting the plot lingo functioanlity library

---
+++ PlotLingoFunctionality/Plots/PlotMaker.cs
@@ -0,0 +1,18 @@
+﻿using PlotLingoLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlotLingoFunctionality.Plots
+{
+    /// <summary>
+    /// Functions and methods having to do with plots
+    /// </summary>
+    [Export(typeof(IFunctionObject))]
+    class PlotMaker : IFunctionObject
+    {
+    }
+}
