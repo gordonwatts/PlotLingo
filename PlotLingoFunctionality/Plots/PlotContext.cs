2d6a1ad8c2d505d1027752a97827c80efed9e1b7
Gordon Watts <gordon@gordonandpaula.net>
2013-11-24 21:03:02 -0800

    Added a dummy title changer... for now...

--- PlotLingoFunctionality/Plots/PlotContext.cs
+++ PlotLingoFunctionality/Plots/PlotContext.cs
@@ -9,7 +9,7 @@ namespace PlotLingoFunctionality.Plots
     public class PlotContext
     {
         /// <summary>
-        /// The list of plots we are most insterested in.
+        /// The list of plots we are most insterested in. This, basically, preresents a gPad...
         /// </summary>
         private ROOTNET.NTH1[] _plots;
 
@@ -20,6 +20,16 @@ namespace PlotLingoFunctionality.Plots
         public PlotContext(ROOTNET.NTH1[] nTH1)
         {
             this._plots = nTH1;
+        }
+
+        /// <summary>
+        /// Alter the title of the canvas.
+        /// </summary>
+        /// <param name="title"></param>
+        /// <returns></returns>
+        public PlotContext Title (string title)
+        {
+            return this;
         }
     }
 }
