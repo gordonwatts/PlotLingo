713c478e026fc1b8c58ce20d4d0628ce88e5b431
Gordon Watts <gordon@gordonandpaula.net>
2013-12-03 23:54:51 -0800

    Add the ability to add histograms together.

---
+++ PlotLingoFunctionality/Plots/PlotOperations.cs
@@ -0,0 +1,25 @@
+﻿using PlotLingoLib;
+using System.ComponentModel.Composition;
+
+namespace PlotLingoFunctionality.Plots
+{
+    /// <summary>
+    /// Define some operations for the language that work on plots. Note we clone everything!
+    /// </summary>
+    [Export(typeof(IFunctionObject))]
+    class PlotOperations : IFunctionObject
+    {
+        /// <summary>
+        /// Add two histograms
+        /// </summary>
+        /// <param name="h1"></param>
+        /// <param name="h2"></param>
+        /// <returns></returns>
+        public static ROOTNET.Interface.NTH1 OperatorPlus(ROOTNET.Interface.NTH1 h1, ROOTNET.Interface.NTH1 h2)
+        {
+            var clone = h1.Clone() as ROOTNET.Interface.NTH1;
+            clone.Add(h2);
+            return clone;
+        }
+    }
+}
