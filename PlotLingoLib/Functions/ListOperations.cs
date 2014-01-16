cdca11b7c36c2dd8f08d97a45f194e4dcdbea991
Gordon Watts <gordon@gordonandpaula.net>
2014-01-16 00:49:52 -0800

    Sum over an array

---
+++ PlotLingoLib/Functions/ListOperations.cs
@@ -0,0 +1,30 @@
+﻿using PlotLingoLib.Expressions;
+using PlotLingoLib.Expressions.Values;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+
+namespace PlotLingoLib.Functions
+{
+    [Export(typeof(IFunctionObject))]
+    class ListOperations : IFunctionObject
+    {
+        /// <summary>
+        /// Add the values up!
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static object sum(IEnumerable<object> input)
+        {
+            // Since these are already evalutaed objects (or should be), we don't
+            // need a real context.
+            var tmp = new RootContext();
+            var r = input.Aggregate((v1, v2) =>
+            {
+                var fo = new FunctionExpression("+", new IExpression[] { new ObjectValue(v1), new ObjectValue(v2) });
+                return fo.Evaluate(tmp);
+            });
+            return r;
+        }
+    }
+}
---
+++ PlotLingoLibTest/Functions/ListOperationsTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlotLingoLib;
+using PlotLingoLib.Expressions;
+using PlotLingoLib.Expressions.Values;
+
+namespace PlotLingoLibTest.Functions
+{
+    [TestClass]
+    public class ListOperationsTest
+    {
+        [TestMethod]
+        public void SumEmptyObjectArray()
+        {
+            var ar = new object[] { };
+            var f = new FunctionExpression("sum", new ObjectValue(ar));
+
+            var c = new RootContext();
+            var r = f.Evaluate(c);
+
+            Assert.IsNull(r);
+        }
+
+        [TestMethod]
+        public void SumSingleObjectArray()
+        {
+            var ar = new object[] { 5 };
+            var f = new FunctionExpression("sum", new ObjectValue(ar));
+
+            var c = new RootContext();
+            var r = f.Evaluate(c);
+
+            Assert.AreEqual(5, r);
+        }
+
+        [TestMethod]
+        public void SumDoubleObjectArray()
+        {
+            var ar = new object[] { 5, 6 };
+            var f = new FunctionExpression("sum", new ObjectValue(ar));
+
+            var c = new RootContext();
+            var r = f.Evaluate(c);
+
+            Assert.AreEqual(11, r);
+        }
+    }
+}
