7c019f5dddc76475ca897437fe67f410eb614b6e
Gordon Watts <gordon@gordonandpaula.net>
2014-12-02 09:28:20 +0100

    Should never have been added...

--- start_PlotLingo.bat
+++
@@ -1,4 +0,0 @@
-@REM Use to start devenv
-@set ROOTSYS=C:\Users\Gordon\Documents\Code\PlotLingo\\root\root-5.34.21-vc11
-@set PATH=C:\Users\Gordon\Documents\Code\PlotLingo\\root\root-5.34.21-vc11\bin;%PATH%
-@C:\Users\Gordon\Documents\Code\PlotLingo\PlotLingo.sln
