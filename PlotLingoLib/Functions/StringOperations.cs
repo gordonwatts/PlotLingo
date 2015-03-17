98dee39bdf742f6a5a3589141c13bb3beb4c6047
Gordon Watts <gordon@gordonandpaula.net>
2015-03-17 15:17:03 +0100

    Add ability to add strings together

---
+++ PlotLingoLib/Functions/StringOperations.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.Composition;
+
+namespace PlotLingoLib.Functions
+{
+    /// <summary>
+    /// Some basic string operations.
+    /// </summary>
+    [Export(typeof(IFunctionObject))]
+    class StringOperations : IFunctionObject
+    {
+        /// <summary>
+        /// Sum strings.
+        /// </summary>
+        /// <param name="ctx"></param>
+        /// <param name="o1"></param>
+        /// <param name="o2"></param>
+        /// <returns></returns>
+        public static string OperatorPlus(IScopeContext ctx, string o1, string o2)
+        {
+            return o1 + o2;
+        }
+    }
+}
---
+++ PlotLingoLibTest/Functions/StringOperationsTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlotLingoLib;
+using PlotLingoLib.Expressions;
+using PlotLingoLib.Expressions.Values;
+
+namespace PlotLingoLibTest.Functions
+{
+    [TestClass]
+    public class StringOperationsTest
+    {
+        [TestMethod]
+        public void SimpleStringPlus()
+        {
+            var cl = new FunctionExpression("+", new StringValue("hi"), new StringValue(" there"));
+            var c = new RootContext();
+            var r = cl.Evaluate(c);
+            Assert.IsInstanceOfType(r, typeof(string), "type of result");
+            var rut = r as string;
+            Assert.AreEqual("hi there", rut);
+        }
+    }
+}
