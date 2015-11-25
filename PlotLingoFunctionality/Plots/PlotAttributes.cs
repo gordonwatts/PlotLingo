c5e31bb6579d0efac28664c7a7584c45d8ef6c29
Gordon Watts <gordon@gordonandpaula.net>
2015-11-25 22:07:21 +0100

    Minor updates
    - Add a logy
    - Don't normalize plots by default

---
+++ PlotLingoFunctionality/Plots/PlotAttributes.cs
@@ -0,0 +1,23 @@
+﻿using PlotLingoLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlotLingoFunctionality.Plots
+{
+    /// <summary>
+    /// Some fun utilities for dealing with plots.
+    /// </summary>
+    [Export(typeof(IFunctionObject))]
+    class PlotAttributes : IFunctionObject
+    {
+        public static PlotContext logy(IScopeContext c, PlotContext ctx)
+        {
+            ctx.AddPostplotHook((myctx, canvas) => { canvas.Logy = 1; });
+            return ctx;
+        }
+    }
+}
