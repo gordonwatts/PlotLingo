757c075140e15561a48d1b4da95a362a7a6b3d57
Gordon Watts <gordon@gordonandpaula.net>
2015-11-25 16:56:22 +0100

    Add ability to plot Graph's

---
+++ PlotLingoFunctionality/Plots/GraphContext.cs
@@ -0,0 +1,92 @@
+﻿using PlotLingoLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using ROOTNET.Interface;
+
+namespace PlotLingoFunctionality.Plots
+{
+    /// <summary>
+    /// Generate plots for various graphs
+    /// </summary>
+    public class GraphContext : IPlotScriptResult
+    {
+        NTGraph[] _g;
+
+        /// <summary>
+        /// Initialize with a full list of graphs
+        /// </summary>
+        /// <param name="nTGraph"></param>
+        public GraphContext(NTGraph[] nTGraph)
+        {
+            _g = nTGraph;
+        }
+
+        public GraphContext(NTGraph g)
+        {
+            _g = new NTGraph[] { g };
+        }
+
+        /// <summary>
+        /// Get the name of the plot for drawing
+        /// </summary>
+        public string Name
+        {
+            get { return _g[0].Name; }
+        }
+
+        /// <summary>
+        /// Save to a file.
+        /// </summary>
+        /// <param name="ctx"></param>
+        /// <param name="filenameStub"></param>
+        /// <returns></returns>
+        public IEnumerable<FileInfo> Save(IScopeContext ctx, string filenameStub)
+        {
+            // Initialize the canvas
+            var c = new ROOTNET.NTCanvas();
+            c.Title = _g[0].Title;
+
+            // Plot everything.
+            var optS = "";
+            foreach (var p in _g)
+            {
+                p.Draw(optS);
+                optS = "SAME";
+            }
+
+            // Save it
+            string[] formats = null;
+            var formatsExpr = ctx.GetVariableValue("plotformats") as object[];
+            if (formatsExpr != null)
+                formats = formatsExpr.Cast<string>().ToArray();
+            if (formats == null)
+                formats = new string[] { "png" };
+
+            var finfos = formats
+                .Select(fmt =>
+                {
+                    var fout = new FileInfo(string.Format("{0}.{1}", filenameStub, fmt));
+                    if (fout.Exists)
+                    {
+                        try
+                        {
+                            fout.Delete();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Had trouble removing the old file {0}", fout.FullName);
+                            Console.WriteLine("  -> {0}", e.Message);
+                        }
+                    }
+                    c.SaveAs(fout.FullName);
+                    return fout;
+                });
+            return finfos.ToArray();
+        }
+    }
+}
---
+++ PlotLingoFunctionality/Plots/GraphMaker.cs
@@ -0,0 +1,34 @@
+﻿using PlotLingoLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlotLingoFunctionality.Plots
+{
+    [Export(typeof(IFunctionObject))]
+    class GraphMaker : IFunctionObject
+    {
+        /// <summary>
+        /// Return a graph plot
+        /// </summary>
+        /// <param name="g"></param>
+        /// <returns></returns>
+        public static GraphContext draw(ROOTNET.NTGraph g)
+        {
+            return new GraphContext(g);
+        }
+
+        /// <summary>
+        /// Draw a bunch of them.
+        /// </summary>
+        /// <param name="plotList"></param>
+        /// <returns></returns>
+        public static GraphContext draw(IEnumerable<object> plotList)
+        {
+            return new GraphContext(plotList.Cast<ROOTNET.Interface.NTGraph>().ToArray());
+        }
+    }
+}
