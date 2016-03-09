438616426a4dc302a31e0370ca8fea2d0ddad6a3
Gordon Watts <gordon@gordonandpaula.net>
2016-03-08 23:18:47 -0600

    Remove back slashes from the output too

--- PlotLingoConsole/Utils.cs
+++ PlotLingoConsole/Utils.cs
@@ -18,6 +18,7 @@ namespace PlotLingoConsole
                 .Replace("?", "-")
                 .Replace("\"", "-")
                 .Replace("|", "-")
+                .Replace("\\", "")
                 ;
         }
     }
