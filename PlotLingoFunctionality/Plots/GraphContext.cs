a3c4eb0f1026fc43113433eff3b40cc627a3fbad
Gordon Watts <gordon@gordonandpaula.net>
2016-04-27 00:00:09 -0700

    First go at a more full graf context

--- PlotLingoFunctionality/Plots/GraphContext.cs
+++ PlotLingoFunctionality/Plots/GraphContext.cs
@@ -16,6 +16,11 @@ namespace PlotLingoFunctionality.Plots
     public class GraphContext : IPlotScriptResult
     {
         NTGraph[] _g;
+        string _xaxisTitle = "";
+        string _yaxisTitle = "";
+        string _title = "";
+        bool _logx = false;
+        bool _logy = false;
 
         /// <summary>
         /// Initialize with a full list of graphs
@@ -36,7 +41,74 @@ namespace PlotLingoFunctionality.Plots
         /// </summary>
         public string Name
         {
-            get { return _g[0].Name; }
+            get { return string.IsNullOrWhiteSpace(_title) ? _g[0].Name : _title; }
+        }
+
+        public GraphContext yaxis(string yaxisName)
+        {
+            _yaxisTitle = yaxisName;
+            return this;
+        }
+
+        public GraphContext xaxis(string yaxisName)
+        {
+            _xaxisTitle = yaxisName;
+            return this;
+        }
+
+        public GraphContext title(string t)
+        {
+            _title = t;
+            return this;
+        }
+
+        public GraphContext logx(bool doit = true)
+        {
+            _logx = doit;
+            return this;
+        }
+        public GraphContext logy(bool doit = true)
+        {
+            _logy = doit;
+            return this;
+        }
+
+        /// <summary>
+        /// Contains the list of actions to be executed before an actual plot is made.
+        /// These are run just before things are dumped out.
+        /// </summary>
+        private List<Action<IScopeContext, GraphContext>> _prePlotHook = new List<Action<IScopeContext, GraphContext>>();
+
+        /// <summary>
+        /// Track all the things we should call once the plotting is, basically, done.
+        /// </summary>
+        private List<Action<GraphContext, ROOTNET.Interface.NTCanvas>> _postPlotHook = new List<Action<GraphContext, ROOTNET.Interface.NTCanvas>>();
+
+        /// <summary>
+        /// Add a pre-plot hook
+        /// </summary>
+        /// <param name="act"></param>
+        public void AddPreplotHook(Action<GraphContext> act)
+        {
+            _prePlotHook.Add((c, p) => act(p));
+        }
+
+        /// <summary>
+        /// Add a pre-plot hook that needs a context
+        /// </summary>
+        /// <param name="act"></param>
+        public void AddPreplotHook(Action<IScopeContext, GraphContext> act)
+        {
+            _prePlotHook.Add(act);
+        }
+
+        /// <summary>
+        /// Add a hook to be called after the basic plotting is done.
+        /// </summary>
+        /// <param name="act"></param>
+        public void AddPostplotHook(Action<GraphContext, ROOTNET.Interface.NTCanvas> act)
+        {
+            _postPlotHook.Add(act);
         }
 
         /// <summary>
@@ -47,16 +119,43 @@ namespace PlotLingoFunctionality.Plots
         /// <returns></returns>
         public IEnumerable<FileInfo> Save(IScopeContext ctx, string filenameStub)
         {
+            // Do pre-plot actions.
+            foreach (var ph in _prePlotHook)
+            {
+                ph(ctx, this);
+            }
+
             // Initialize the canvas
             var c = new ROOTNET.NTCanvas();
+            if (!string.IsNullOrWhiteSpace(_title))
+            {
+                _g[0].Title = _title;
+            }
             c.Title = _g[0].Title;
+
+            c.Logx = _logx ? 1 : 0;
+            c.Logy = _logy ? 1 : 0;
 
             // Plot everything.
             var optS = "";
             foreach (var p in _g)
             {
+                if (!string.IsNullOrWhiteSpace(_xaxisTitle))
+                {
+                    p.Xaxis.Title = _xaxisTitle;
+                }
+                if (!string.IsNullOrWhiteSpace(_yaxisTitle))
+                {
+                    p.Yaxis.Title = _yaxisTitle;
+                }
                 p.Draw(optS);
                 optS = "SAME";
+            }
+
+            // Post plot actions
+            foreach (var ph in _postPlotHook)
+            {
+                ph(this, c);
             }
 
             // Save it
