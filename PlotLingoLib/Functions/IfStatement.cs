875b11d7772f403fa1b96ce5bfda59d88c0aca02
Gordon Watts <gordon@gordonandpaula.net>
2016-07-03 22:17:19 +0200

    Add an if function (no else yet)

---
+++ PlotLingoLib/Functions/IfStatement.cs
@@ -0,0 +1,46 @@
+﻿using PlotLingoLib.Expressions;
+using PlotLingoLib.Expressions.Values;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlotLingoLib.Functions
+{
+    /// <summary>
+    /// Implement an inline if statemnet (a "?" operator written out the long way).
+    /// </summary>
+    [Export(typeof(IFunctionObject))]
+    class IfStatement : IFunctionObject
+    {
+        /// <summary>
+        /// Simple if statement.
+        /// </summary>
+        /// <param name="ctx"></param>
+        /// <param name="logicalTest"></param>
+        /// <param name="statements"></param>
+        /// <returns></returns>
+        public static object ifReserved(IScopeContext ctx, IExpression logicalTest, ListOfStatementsExpression statements)
+        {
+            // See if we have fired
+            var testResult = logicalTest.Evaluate(ctx);
+            if (testResult.GetType() != typeof(bool) && testResult.GetType() != typeof(int))
+            {
+                throw new ArgumentException($"The test {logicalTest.ToString()} did not evaluate to an integer or a boolean");
+            }
+
+            var shouldExecute = testResult.GetType() == typeof(bool)
+                ? (bool)testResult
+                : (int)testResult != 0;
+            
+            if (shouldExecute)
+            {
+                var newScope = new ScopeContext(ctx);
+                return statements.Evaluate(newScope);
+            }
+            return null;
+        }
+    }
+}
---
+++ PlotLingoLibTest/Functions/ifStatementTest.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlotLingoLib;
+using PlotLingoLib.Expressions;
+using PlotLingoLib.Expressions.Values;
+using PlotLingoLib.Functions;
+using PlotLingoLib.Statements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlotLingoLibTest.Functions
+{
+    [TestClass]
+    public class ifStatementTest
+    {
+        [TestMethod]
+        public void SimpleTrueStatement()
+        {
+            var testExpr = new IntegerValue(1);
+            var assign = new AssignmentStatement("a", new IntegerValue(10));
+            var statement = new ListOfStatementsExpression(new IStatement[] { assign });
+
+            var ctx = new RootContext();
+            ctx.SetVariableValue("a", 5);
+
+            var ifStatement = new FunctionExpression("if", testExpr, statement);
+            ifStatement.Evaluate(ctx);
+
+            Assert.AreEqual(10, ctx.GetVariableValue("a"));
+        }
+
+        [TestMethod]
+        public void SimpleFalseStatement()
+        {
+            var testExpr = new IntegerValue(0);
+            var assign = new AssignmentStatement("a", new IntegerValue(10));
+            var statement = new ListOfStatementsExpression(new IStatement[] { assign });
+
+            var ctx = new RootContext();
+            ctx.SetVariableValue("a", 5);
+
+            var ifStatement = new FunctionExpression("if", testExpr, statement);
+            ifStatement.Evaluate(ctx);
+
+            Assert.AreEqual(5, ctx.GetVariableValue("a"));
+        }
+
+        [TestMethod]
+        public void ExecuteInDifferentContex()
+        {
+            var testExpr = new IntegerValue(1);
+            var assign = new AssignmentStatement("b", new IntegerValue(10));
+            var statement = new ListOfStatementsExpression(new IStatement[] { assign });
+
+            var ctx = new RootContext();
+            ctx.SetVariableValue("a", 5);
+
+            var ifStatement = new FunctionExpression("if", testExpr, statement);
+            ifStatement.Evaluate(ctx);
+
+            Assert.IsFalse(ctx.GetVariableValueOrNull("b").Item1);
+        }
+    }
+}
